c691b9e37b7124f77b3866228a78f52e876254f3
Jerson <valdezjerson8624@gmail.com>
2025-12-15 09:03:23 +0800

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-gray-100">
+      <div className="rounded-xl bg-blue-600 p-10 text-white shadow-2xl">
+        <h1 className="text-4xl font-bold">Tailwind v4 is Working!</h1>
+        <p className="mt-2 text-blue-200">Lutong Paombong Project</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
